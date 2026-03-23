3061df34f835c6d47e80d8b24bf491910edd6fd4
Lukas <lukas-wagner@aon.at>
2026-03-23 18:47:51 +0100

    stretching included in project settings

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1773942561|7243547657';
+const CACHE_VERSION = '1774288050|154890743';
 /** @type {string} */
 const CACHE_PREFIX = 'Dobble-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
