d14d54be51733c4c0e2d679165b14fc8a5840a70
Lukas <lukas-wagner@aon.at>
2026-03-23 20:07:37 +0100

    s

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1774292477|4581855932';
+const CACHE_VERSION = '1774292816|4920521735';
 /** @type {string} */
 const CACHE_PREFIX = 'Dobble-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
